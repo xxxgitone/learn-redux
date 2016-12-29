37cddc7c1f5f2722d3643cc7cf5778adaa0bf8c8
xxxgitone <xxxgit@sina.com>
2016-12-29 16:50:41 +0800

    环境搭建，路由配置

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const PhotoGrid = React.createClass({
+	render(){
+		return(
+			<div className="photo-grid">
+				I'm a photo grid.
+			</div>
+		);
+	}
+});
+
+export default PhotoGrid;
