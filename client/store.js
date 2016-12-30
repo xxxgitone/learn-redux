38e46a94035a137ebddb581172f6bf97af4edf70
xxxgitone <xxxgit@sina.com>
2016-12-30 17:50:58 +0800

    am配合使用Redux DevTools

--- client/store.js
+++ client/store.js
@@ -1,4 +1,4 @@
-import { createStore, compse } from 'redux';
+import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
@@ -14,7 +14,12 @@ const defaultState = {
 	comments
 }
 
-const store = createStore(rootReducer, defaultState);
+//配合使用Redux DevTools
+const enhancers = compose(
+	window.devToolsExtension ? window.devToolsExtension() : f => f 
+);
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
